105980cfd9d9772699100cf25bd077355035756a
ckarthan <S00047217@acad.tri-c.edu>
2019-01-25 16:27:32 -0500

    Initial check in Lab 2

---
+++ CallisMVCBookstore/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallisMVCBookstore.Controllers {
+    public class ProductController : Controller {
+        // GET: Product
+        public ActionResult Index() {
+            ViewBag.Message = "Product/Index is displayed";
+
+            return View();
+        }
+
+        //GET Browse
+        public string Browse() {
+            string browseDisplay = "Browse displayed";
+
+            return browseDisplay;
+        }
+
+        //GET Details with ID param
+        public string Details(int productId) {
+            string productDetails = "Details displayed for Id: " + productId;
+
+            return productDetails;
+        }
+
+        public string Location(int zipcode) {
+            string locationValue = HttpUtility.HtmlEncode("Location displayed for zip: " + zipcode);
+
+            return locationValue;
+        }
+    }
+}
---
+++ CallisMVCBookstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CallisMVCBookstore.Startup))]
+namespace CallisMVCBookstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
