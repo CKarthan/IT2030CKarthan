280c680f8c4ea1e2113e2bdcc3b4a800723b69fc
ckarthan <S00047217@acad.tri-c.edu>
2019-03-31 13:54:21 -0400

    Added Stre Front views Lab 10

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreFrontController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreFrontController : Controller
+    {
+
+        MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: StoreFront
+        public ActionResult Index(int id){
+            var genreAlbums = db.Albums.Where(a => a.GenreId == id).ToList();
+            return View("Index", genreAlbums);
+        }
+
+        public ActionResult Browse() {
+            var genres = db.Genres.ToList().OrderBy(g => g.Name);
+            return View("Browse", genres);
+        }
+
+        public ActionResult Details(int id) {
+            var album = db.Albums.FirstOrDefault(a => a.AlbumId == id);
+            return View("Details", album);
+        }
+
+    }
+}
